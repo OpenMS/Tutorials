52cff6325442bc834fffb0a83cbcb5114e5f60ae
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2019-06-24 22:44:42 +0200

    Update runner script (only basic_ID workflow for now)

--- Workflows/executeBatch.sh
+++ Workflows/executeBatch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for wf in testIn*.knwf
+for wf in basic_peptide_*.knwf
 do
  "${1}" -consoleLog -nosplash -data "${2}" -application org.knime.product.KNIME_BATCH_APPLICATION -workflowFile="$(pwd)/${wf}" -reset  > log_${wf}.txt
 done
